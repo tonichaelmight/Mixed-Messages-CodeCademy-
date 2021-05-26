db7d10f1d303f597c5afcaf1b12576c4dbeb8e78
tonichaelmight <tonichaelmight@gmail.com>
2021-05-26 01:52:43 -0400

    Update index.js
    add titles

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // CodeCademy FullStack Track
 // 26 May 2021
 
-/* 
+/* PROJECT PROMPT
 For this project, you will build a message generator program. 
 Every time a user runs a program, they should get a new, randomized output. 
 You’re welcome to take the project in a couple of different forms, like an astrology generator, 
@@ -12,3 +12,53 @@ To make your program truly random, the message that it outputs should be made up
 at least three different pieces of data. 
 Take what you know of JavaScript syntax so far to build the program and customize it to your liking.
 */
+
+// main function generateRandomMessage() will take a name as an argument, interpolating it with three random pieces, one from each array below.
+
+// titles
+const beginnings = [
+    'Prince',
+    'Princess',
+    'Duke',
+    'Duchess',
+    'Grand Duke',
+    'Grand Duchess',
+    'Marquis',
+    'Marquess',
+    'Count',
+    'Countess',
+    'Earl',
+    'Viscount',
+    'Viscountess',
+    'Baron',
+    'Baroness',
+    'Lord',
+    'Lady',
+    'Emporer',
+    'Empress',
+    'King', 
+    'Queen'
+];
+
+
+const middles = [];
+
+const ends = [];
+
+const generateRandomIndex = arr => {
+
+    return Math.floor(Math.random() * arr.length);
+
+};
+
+const generateRandomMessage = (name) => {
+
+    const beginning = beginnings[generateRandomIndex(beginnings)];
+    const middle = middles[generateRandomIndex(middles)];
+    const end = ends[generateRandomIndex(ends)];
+
+    console.log(`${beginning} ${name}, ${middle} of ${end}.`)
+
+};
+
+generateRandomMessage();
