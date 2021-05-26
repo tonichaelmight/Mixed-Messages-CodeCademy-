85545906a2e17adab29798d9654e49310b538f67
tonichaelmight <tonichaelmight@gmail.com>
2021-05-26 02:09:42 -0400

    Update index.js
    Add comments

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ Take what you know of JavaScript syntax so far to build the program and customiz
 
 // main function generateRandomMessage() will take a name as an argument, interpolating it with three random pieces, one from each array below.
 
-// beginnings
+// titles
 const beginnings = [
     'Prince',
     'Princess',
@@ -40,36 +40,50 @@ const beginnings = [
     'Queen'
 ];
 
-
+// 'job' words
 const middles = [
     'Slayer',
     'Curator',
     'Creator',
-    'Master'
+    'Master',
+    'Teacher',
+    'Forger'
 ];
 
+// dramatic nouns
 const ends = [
     'Dragons',
     'Swords',
     'Darkness',
-    'Light'
+    'Light',
+    'Hearts'
 ];
 
+
+// function to general a random index of an array, regardless of array's length
 const generateRandomIndex = arr => {
 
     return Math.floor(Math.random() * arr.length);
 
 };
 
+
+// generates a random messages
 const generateRandomMessage = (name) => {
 
+    // get a random index from each array and save it
     const beginning = beginnings[generateRandomIndex(beginnings)];
     const middle = middles[generateRandomIndex(middles)];
     const end = ends[generateRandomIndex(ends)];
 
+    // log the message
     console.log(`${beginning} ${name}, ${middle} of ${end}.`)
 
 };
+
+
+
+// TEST
 
 // me n all my homies
 const names = [
@@ -90,3 +104,19 @@ names.forEach(name => {
     generateRandomMessage(name);
 });
 console.log(' ');
+
+// SAMPLE OUTPUT
+/*
+
+Count Michael, Creator of Darkness.
+Empress Matt, Slayer of Light.
+Viscountess Bekka, Creator of Swords.
+Grand Duke Danielle, Master of Darkness.
+Prince Amanda, Slayer of Dragons.
+Viscountess Kayla, Creator of Darkness.
+Grand Duchess Nathan, Creator of Dragons.
+Prince Jackie, Curator of Darkness.
+Marquis Hannah, Creator of Dragons.
+Countess Signe, Slayer of Light.
+
+*/
